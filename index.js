afc0eee95427df4c66e182ba86acb9dd18e31e1e
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-06-30 09:45:45 -0700

    feat: implement logic to choose between operations
    Use switch statement to perform operation based on operator.

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+const ADDITION_OPERATOR = "+";
+const SUBTRACTION_OPERATOR = "-";
+const MULTIPLICATION_OPERATOR = "*";
+const DIVISION_OPERATOR = "/";
+
 let leftOperand;
 let operator;
 let rightOperand;
+
+function operate(leftOperand, operator, rightOperand) {
+    switch(operator) {
+        case ADDITION_OPERATOR:
+            return add(leftOperand, rightOperand);
+        case SUBTRACTION_OPERATOR:
+            return subtract(leftOperand, rightOperand);
+        case MULTIPLICATION_OPERATOR:
+            return multiply(leftOperand, rightOperand);
+        case DIVISION_OPERATOR:
+            return divide(leftOperand, rightOperand);
+    }
+}
 
 function add(a, b) {
     return a + b;
